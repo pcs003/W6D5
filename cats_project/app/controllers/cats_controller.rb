9b0ba0d39844f2c7374b094931cd0450370309ca
Parth <pcs003@ucsd.edu>
2021-02-05 16:18:58 -0500

    complete new and edit form along with create and update controller method

--- cats_project/app/controllers/cats_controller.rb
+++ cats_project/app/controllers/cats_controller.rb
@@ -8,4 +8,36 @@ class CatsController < ApplicationController
         @cat = Cat.find_by(id: params[:id])
         render :show
     end
+
+    def new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            render :show
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+        if @cat.update(cat_params)
+            render :show
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
+    end
 end
--- cats_project/app/models/cat.rb
+++ cats_project/app/models/cat.rb
@@ -1,5 +1,5 @@
 class Cat < ApplicationRecord
-
+    COLORS = ['red','orange','yellow','green','blue','purple','white','brown','black']
     validates :birth_date, :color, :name, :sex, presence:true
 
     
